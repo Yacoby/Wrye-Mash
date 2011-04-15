4bf2231dcde1f6178e5ac51934c6ab1895e20fcf
Jacob Essex <jacob@jacobessex.com>
2011-04-15 14:16:55 +0100

    Removed dead code, added/updated comments

--- Mopy/wtexparser.py
+++ Mopy/wtexparser.py
@@ -14,8 +14,8 @@ def dfFlattenNodeTree(heading, maxLevel=0):
 def getHtmlFromHeadings(headings):
     """Generates HTML for a heading and all decendents based on Wrye's format"""
 
-    def htmlDispatch(obj,  prop, curText):
-        """ This function is passed into the text to transform it depening on text properties """
+    def htmlDecorator(obj,  prop, curText):
+        """ This function is passed into the text to decorate it depening on text properties """
         mapping = {
             'bold'   : lambda t : '<strong>' + t + '</strong>', 
             'italic' : lambda t : '<em>' + t + '</em>', 
@@ -28,7 +28,7 @@ def getHtmlFromHeadings(headings):
     def getHtmlFromLine(line):
         html = ''
         for text in line:
-            html += text.transform(htmlDispatch)
+            html += text.decorate(htmlDecorator)
         return html
 
     def getHtmlFromHeading(heading):
@@ -44,10 +44,8 @@ def getHtmlFromHeadings(headings):
    
 class Text:
     """
-        A class that holds properties of an text, and can merge with other text
+        A class that holds properties of an text, and can merge with other Text objects
     """
-
-
     def __init__(self, text):
         self.text   = text
         self.bold   = False
@@ -55,16 +53,17 @@ class Text:
 
     def mergeWith(self, text):
         """ This merges two Text classes, however, if a property exists in the object the function belongs to
-            it won't be overwitten by text unless it is False"""
+            it won't be overwitten by text unless it is False. (This is to account for properties such as bold)
+        """
         for name, val in text.__dict__.iteritems():
             if name in self.__dict__:
                 self.__dict__[name] = self.__dict__[name] or val
             else:
                 self.__dict__[name] = val
 
-    def transform(self, function):
+    def decorate(self, function):
         """ 
-        Transforms the text by passing each property through the given function.
+        Decorates the text by passing each property through the given function.
         The function should be of the form
             function(textObject, propertyName, currentTextToModify)
         """
@@ -80,7 +79,6 @@ class Node:
         self.children = []
         self.parent   = parent
 
-
 class HeadingNode(Node):
     """
     This is a bit confusing. However, for my sanity
@@ -93,9 +91,13 @@ class HeadingNode(Node):
         Node.__init__(self, parent, level)
         self.title = title 
 
+        #The root node for all text stemming from this heading
         self.textNode     = None 
 
     def getTextLines(self):
+        """
+            returns a generator contain all the text in this node
+        """
         if self.textNode == None:
             return
         for n in dfFlattenNodeTree(self.textNode):
@@ -104,6 +106,11 @@ class HeadingNode(Node):
 
 class TextNode(Node):
     def __init__(self, parent, level, text):
+        """
+            parent: the parent node
+            level : the distance down the tree, assuming the root is 0
+            text  : a list of Text objects
+        """
         Node.__init__(self, parent, level)
         self.text = text
 
@@ -115,12 +122,12 @@ class TextNode(Node):
         return result
 
 class Parser:
-    """A rough parser for Wrye's wtex format. It doesn't handle it as well as his converter
+    """A rough parser for Wrye's wtex format. It doesn't handle it as quite as well as his converter
     
-    Really, it is a bit of a mess. It parses headings into a tree of nodes. Each heading
-    can contain a tree of text nodes below it (not connected to the heading node tree).
+    It parses headings into a tree of nodes. Each heading can contain a tree of text nodes below 
+    it (not connected to the heading node tree).
 
-    Each text node contains the parsed text
+    Each text node contains the parsed text, where each node corisponds to a line
     """
     def __init__(self):
         self.root = self.currentHeading = Node(None, 0);
@@ -137,11 +144,6 @@ class Parser:
         """Gives a generator of all headings"""
         for h in dfFlattenNodeTree(self.root, maxLevel):
             yield h
-
-    def parseFile(self, wtex):
-        wtexFile = open(wtex)
-        for line in wtex.readlines():
-            self.parseLine(line)
 
     def parseString(self, wtex):
         for line in wtex.split('\n'):
@@ -165,7 +167,8 @@ class Parser:
                                           lambda p: HeadingNode(p, level, text))
         assert self.currentHeading!=None, "The current heading shouldn't be None"
 
-        #we have stopped parsing text, so flag this 
+        #we have stopped parsing text, so flag this. This will cause the
+        #text to be added to the new current node rather than the old one
         self.currentText = None
 
     def insert(self, currentNode, level, creator):
@@ -188,9 +191,7 @@ class Parser:
         elif level <= currentNode.level:
             assert currentNode.parent != None, "At " + str(currentNode.level) + ", there was no parent when searching for level " + str(level-1)
             return self.insert(currentNode.parent, level, creator)
-        #we don't know how to move down the tree, so we can't do this
         elif level > currentNode.level:
-            #raise Exception("Failed as " + str(level) + " was less than the current level of " + str(currentNode.level))
             #we don't really know how to move down the tree, but we shal cheat and add it at the best possible level
             newNode       = creator(currentNode)
             newNode.level = currentNode.level + 1 
@@ -201,13 +202,11 @@ class Parser:
 
     
     def parseText(self, text):
-        """This seperates text into bold, italic, links etc and returns it as a list"""
+        """This seperates text into bold, italic, links etc and returns it as a list. It supports nested formatting"""
         origanalText = text
         result       = []
 
         #matches bold, italic and both
-        result = []
-
         formattingRegex = '__(.*)__'                \
                         + '|'                       \
                         + '~~(.*)~~'                \
@@ -256,23 +255,26 @@ class Parser:
                 break
             text = text[matchLength:]
 
-        #our base case, if the text result is exactly the same as the input, then 
+        #our base case, if the matched text result is exactly the same as the input, then 
         #we assume that there is nothing more to parse
         if len(result) == 1 and result[0].text == origanalText:
             return result
 
-        #at all the text, look down a level and merge if required
+        #at all the text, look down a level at the text within each match and merge if required
         mergedResults = []
         for r in result:
             newResults = self.parseText(r.text)
             #copy the things from this level downwards
             for newResult in newResults:
-                newResult.mergeWith(r) #copy properties down
+                newResult.mergeWith(r) #copy properties down from this level to the one below
                 mergedResults.append(newResult)
 
         return mergedResults 
 
     def parseTextLine(self, line):
+        """ Parses a line of text that is either text, or starts "   * TEXT", where the number of spaces before the * indicates
+            the indentation level
+        """
         match = re.match('([\\s]*)\\* (.+)', line)
 
         if self.currentHeading.textNode == None or self.currentText == None:
@@ -285,7 +287,7 @@ class Parser:
             self.currentText.children.append(node)
         else:
             #given that the lowest level is 0, we need to add one to this
-            #as else it runs into problems. This should have thrown an error. TODO
+            #as else it runs into problems.
             level = len(match.group(1)) + 1 
             text  = self.parseText(match.group(2))
 
