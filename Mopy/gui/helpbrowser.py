c3fac3d8ee9b934b2033713639d5158596567897
Jacob Essex <jacob@jacobessex.com>
2011-04-15 23:10:09 +0100

    Moved helpbrowser code out of mash.py

---
+++ Mopy/gui/helpbrowser.py
@@ -0,0 +1,141 @@
+import wx
+import wx.html
+import wtexparser
+import os
+from balt import spacer, hSizer, vSizer, hsbSizer, vsbSizer, leftSash
+from mosh import _
+
+class TocHtmlWindow(wx.TreeCtrl):
+    """Contains the table of content of the help file.
+    Typically, this is the left panel of the help window."""
+
+    def SetHtmlData(self, data):
+        """data : str : html text"""
+        p = wtexparser.Parser()
+        p.parseString(data)
+
+        if  len(p.root.children) == 1:
+            child = p.root.children[0]
+            self.treeRoot = self.AddRoot(child.title)
+            self.AddToTree(child, self.treeRoot)
+        else:
+            self.treeRoot = self.AddRoot('Help')
+            self.AddToTree(self.root, self.treeRoot)
+        self.Expand(self.treeRoot)
+        self.SelectItem(self.treeRoot, True)
+
+    def AddSelListener(self, f):
+        """
+            This, when given a funcction will call that function with the name of the
+            newly selected item when the selection changes
+        """
+        def PrepForSending(func, event):
+            item = event.GetItem()
+            func(self.GetItemText(item))
+
+        self.Bind(wx.EVT_TREE_SEL_CHANGED, lambda e: PrepForSending(f,e))
+ 
+    def AddToTree(self, docNode, treeNode):
+        for child in docNode.children:
+            newTreeNode = self.AppendItem(treeNode, child.title)
+            self.AddToTree(child, newTreeNode)
+
+
+class HelpPage(wx.html.HtmlWindow):
+    """Class for the pages of the help window.
+    Typically, this is the right panel of the help window."""
+
+    def SetHtmlData(self, data):
+        """data : str : html text"""
+        self.parser = wtexparser.Parser()
+        self.parser.parseString(data)
+
+    def OnLinkClicked(self, link):
+        href = link.GetHref()
+        if href[:1] != '#':
+            wx.LaunchDefaultBrowser(href)
+        else:
+            wx.html.HtmlWindow.OnLinkClicked(self, link)
+
+    def TocSelChanged(self, name):
+        heading = self.parser.getHeading(name) 
+        if heading != None:
+            self.SetPage(wtexparser.getHtmlFromHeadings(heading))
+        else:
+            self.SetPage('')
+
+class HelpBrowser(wx.Frame):
+    """Help Browser frame."""
+    def __init__(self, mashFrame,images, settings):
+        """Intialize."""
+        self.settings = settings
+
+        #--Data
+        self.data = None
+        self.counter = 0
+        #--Singleton
+        global helpBrowser
+        helpBrowser = self
+        #--Window
+        pos  = settings.get('mash.help.pos',(-1,-1))
+        size = settings.get('mash.help.size',(400,600))
+
+        wx.Frame.__init__(self, mashFrame, -1, _('Help'), pos, size, style=wx.DEFAULT_FRAME_STYLE)
+
+        self.SetBackgroundColour(wx.NullColour)
+        self.SetSizeHints(250,250)
+        #--Application Icons
+        self.SetIcons(images['mash.icons2'].GetIconBundle())
+
+        #--Sizers
+        mainSizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        sashPos = 250
+        if 'mash.help.sashPos' in settings:
+            sashPos = settings['mash.help.sashPos']
+        left  = self.left  = leftSash(self,defaultSize=(sashPos,100),onSashDrag=self.OnSashDrag)
+        right = self.right =  wx.Panel(self,style=wx.NO_BORDER)
+
+        self.htmlContent = TocHtmlWindow(left, -1, style = wx.NO_FULL_REPAINT_ON_RESIZE)
+        self.htmlText    = HelpPage(right, -1, style = wx.NO_FULL_REPAINT_ON_RESIZE)
+
+
+        left.SetSizer(vSizer((self.htmlContent, 1, wx.GROW)))
+        right.SetSizer(vSizer((self.htmlText, 1, wx.GROW|wx.ALIGN_RIGHT|wx.EXPAND)))
+
+        mainSizer.Add(left,1,wx.GROW)
+        mainSizer.Add(right,5,wx.GROW)
+        #--Layout
+        self.SetSizer(mainSizer)
+        #--Events
+        wx.EVT_CLOSE(self, self.OnCloseWindow)
+
+        #--Content
+        self.htmlContent.AddSelListener(self.htmlText.TocSelChanged)
+        path = os.path.join(os.getcwd(), 'Wrye Mash.txt')
+        txt  = open(path).read()
+        self.htmlText.SetHtmlData(txt)
+        self.htmlContent.SetHtmlData(txt)
+
+
+        wx.LayoutAlgorithm().LayoutWindow(self, right)
+
+    def OnSashDrag(self,event):
+        """Handle sash moved."""
+        wMin,wMax = 80,self.GetSizeTuple()[0]-80
+        sashPos = max(wMin,min(wMax,event.GetDragRect().width))
+        self.left.SetDefaultSize((sashPos,10))
+        wx.LayoutAlgorithm().LayoutWindow(self, self.right)
+        # screensList.picture.Refresh()
+        self.settings['mash.help.sashPos'] = sashPos
+
+
+    #--Window Closing
+    def OnCloseWindow(self, event):
+        """Handle window close event.
+        Remember window size, position, etc."""
+        self.settings['mash.help.show'] = False
+        if not self.IsIconized() and not self.IsMaximized():
+            self.settings['mash.help.pos'] = self.GetPosition()
+            self.settings['mash.help.size'] = self.GetSizeTuple()
+        self.Destroy()
--- Mopy/masher.py
+++ Mopy/masher.py
@@ -51,7 +51,8 @@ if sys.prefix not in set(os.environ['PATH'].split(';')):
     os.environ['PATH'] += ';'+sys.prefix
 
 try:
-    import wx.lib.iewin
+    pass
+    #import wx.lib.iewin
 except ValueError:
     print 'Failed to import ie. Features may not be available and there may be lots of errrors!'
     import wx.html
@@ -3658,244 +3659,7 @@ class JournalBrowser(wx.Frame):
             settings['mash.journal.size'] = self.GetSizeTuple()
         self.Destroy()
 
-#------------------------------------------------------------------------------
-#-# D.C.-G.
-#
-# Modified in order to display the content on a left panel.
-# TODO: put the content in a notebook, and add indexing
-#    and research functions.
-
-from mysh import HTMLHelpParser
-
-def SetHtmlHelpParser(self, hhp):
-    """Sets the HtmlHelpParser to be used to get the help pages.
-
-    hhp = class mysh.HtmlHelpParser"""
-    self.hhp = hhp
-
-def OnHyperLink(self, event):
-    """..."""
-    if self.target and self.hhp:
-        src = self.hhp.GetPage(event.URL.split("#")[-1])
-        if src[0]: self.target.Navigate(src[0], src[1])
-    event.Cancel = True
-
-
-class DCGHelpBrowser(wx.Frame):
-    """Help Browser frame."""
-    def __init__(self):
-        """Intialize."""
-        #--Data
-        self.data = None
-        self.counter = 0
-        #--Singleton
-        global helpBrowser
-        helpBrowser = self
-        #--Window
-        pos = settings.get('mash.help.pos',(-1,-1))
-        size = settings.get('mash.help.size',(400,600))
-        wx.Frame.__init__(self, mashFrame, -1, _('Help'), pos, size, style=wx.DEFAULT_FRAME_STYLE)
-        self.SetBackgroundColour(wx.NullColour)
-        self.SetSizeHints(250,250)
-        #--Application Icons
-        self.SetIcons(images['mash.icons2'].GetIconBundle())
-        #--Sizers
-        #--Doc fields
-        mainSizer = wx.BoxSizer(wx.HORIZONTAL)
-
-        sashPos = 250
-        left = self.left = leftSash(self,defaultSize=(sashPos,200),onSashDrag=self.OnSashDrag)
-        right = self.right =  wx.Panel(self,style=wx.NO_BORDER)
-
-        #--Doc display
-        self.htmlContent = TOCHtmlWindow(left, -1, style = wx.NO_FULL_REPAINT_ON_RESIZE)
-        self.htmlText    = HelpPage(right, -1, style = wx.NO_FULL_REPAINT_ON_RESIZE)
-        left.SetSizer(vSizer((self.htmlContent, 1, wx.GROW),((10,0),0)))
-        right.SetSizer(vSizer((self.htmlText, 1, wx.GROW|wx.ALIGN_RIGHT|wx.EXPAND)))
-        mainSizer.Add(left,1,wx.GROW)
-        mainSizer.Add(right,1,wx.GROW)
-        #--Layout
-        self.SetSizer(mainSizer)
-        #--Events
-        wx.EVT_CLOSE(self, self.OnCloseWindow)
-        wx.EVT_SIZE(self, self.OnSize)
-        #--Document
-        path = os.path.join(os.getcwd(),'content.html')
-        self.hhp = hhp = HTMLHelpParser()
-        hhp.SetSrc(path)
-        hhp.ReadData()
-        hhp.Parse()
-        self.htmlContent.SetHtmlData(hhp.content)
-        self.htmlContent.SetHtmlHelpParser(hhp)
-        self.htmlContent.Navigate(hhp)
-        self.htmlContent.SetTarget(self.htmlText, defaultView=hhp.GetPage("Intro"))
-        wx.LayoutAlgorithm().LayoutWindow(self, right)
-
-    #--Window Closing
-    def OnCloseWindow(self, event):
-        """Handle window close event.
-        Remember window size, position, etc."""
-        settings['mash.help.show'] = False
-        if not self.IsIconized() and not self.IsMaximized():
-            settings['mash.help.pos'] = self.GetPosition()
-            settings['mash.help.size'] = self.GetSizeTuple()
-        self.Destroy()
-
-    def OnSashDrag(self,event):
-        """Handle sash moved."""
-        wMin,wMax = 80,self.GetSizeTuple()[0]-80
-        sashPos = max(wMin,min(wMax,event.GetDragRect().width))
-        self.left.SetDefaultSize((sashPos,10))
-        wx.LayoutAlgorithm().LayoutWindow(self, self.right)
-        settings['bash.screens.sashPos'] = sashPos
-
-    def OnSize(self,event=None):
-        wx.LayoutAlgorithm().LayoutWindow(self, self.right)
-
-import wtexparser
-class TOCHtmlWindow(wx.TreeCtrl):
-    """Contains the table of content of the help file.
-    Typically, this is the left panel of the help window."""
-
-    def SetHtmlData(self, data):
-        """data : str : html text"""
-        p = wtexparser.Parser()
-        p.parseString(data)
-
-        if  len(p.root.children) == 1:
-            child = p.root.children[0]
-            self.treeRoot = self.AddRoot(child.title)
-            self.AddToTree(child, self.treeRoot)
-        else:
-            self.treeRoot = self.AddRoot('Help')
-            self.AddToTree(self.root, self.treeRoot)
-
-
-    def AddSelListener(self, f):
-        """
-            This, when given a funcction will call that function with the name of the
-            newly selected item when the selection changes
-        """
-        def PrepForSending(func, event):
-            item = event.GetItem()
-            func(self.GetItemText(item))
-
-        self.Bind(wx.EVT_TREE_SEL_CHANGED, lambda e: PrepForSending(f,e))
- 
-    def AddToTree(self, docNode, treeNode):
-        for child in docNode.children:
-            newTreeNode = self.AppendItem(treeNode, child.title)
-            self.AddToTree(child, newTreeNode)
-
-
-class HelpPage(wx.html.HtmlWindow):
-    """Class for the pages of the help window.
-    Typically, this is the right panel of the help window."""
-
-    def SetHtmlData(self, data):
-        """data : str : html text"""
-
-        self.parser = wtexparser.Parser()
-        self.parser.parseString(data)
-
-    def OnLinkClicked(self, link):
-        href = link.GetHref()
-        if href[:1] != '#':
-            wx.LaunchDefaultBrowser(href)
-        else:
-            wx.html.HtmlWindow.OnLinkClicked(self, link)
-
-    def TocSelChanged(self, name):
-        heading = self.parser.getHeading(name) 
-        if heading != None:
-            self.SetPage(wtexparser.getHtmlFromHeadings(heading))
-        else:
-            self.SetPage('')
-
-#-# Original class
-class HtmlHelpBrowser(wx.Frame):
-    """Help Browser frame."""
-    def __init__(self):
-        """Intialize."""
-        #--Data
-        self.data = None
-        self.counter = 0
-        #--Singleton
-        global helpBrowser
-        helpBrowser = self
-        #--Window
-        pos = settings.get('mash.help.pos',(-1,-1))
-        size = settings.get('mash.help.size',(400,600))
-        wx.Frame.__init__(self, mashFrame, -1, _('Help'), pos, size,
-            style=wx.DEFAULT_FRAME_STYLE)
-        self.SetBackgroundColour(wx.NullColour)
-        self.SetSizeHints(250,250)
-        #--Application Icons
-        self.SetIcons(images['mash.icons2'].GetIconBundle())
-        #--Sizers
-        #--Doc fields
-        mainSizer = wx.BoxSizer(wx.HORIZONTAL)
-
-        sashPos = 250
-        if 'mash.help.sashPos' in settings:
-            sashPos = settings['mash.help.sashPos']
-        left = self.left = leftSash(self,defaultSize=(sashPos,100),onSashDrag=self.OnSashDrag)
-        #left  = self.left = leftSash(self,defaultSize=(sashPos,200))
-        right = self.right =  wx.Panel(self,style=wx.NO_BORDER)
-
-
-        self.htmlContent = TOCHtmlWindow(left, -1, style = wx.NO_FULL_REPAINT_ON_RESIZE)
-        self.htmlText    = HelpPage(right, -1, style = wx.NO_FULL_REPAINT_ON_RESIZE)
-
-        self.htmlContent.AddSelListener(self.htmlText.TocSelChanged)
-
-        left.SetSizer(vSizer((self.htmlContent, 1, wx.GROW)))
-        right.SetSizer(vSizer((self.htmlText, 1, wx.GROW|wx.ALIGN_RIGHT|wx.EXPAND)))
-
-        mainSizer.Add(left,1,wx.GROW)
-        mainSizer.Add(right,5,wx.GROW)
-        #--Layout
-        self.SetSizer(mainSizer)
-        #--Events
-        #self.Bind(wx.EVT_SIZE,self.OnSize)
-        wx.EVT_CLOSE(self, self.OnCloseWindow)
-
-        path = os.path.join(os.getcwd(),_('Wrye Mash.html'))
-        self.htmlText.SetPage(open(path).read())
-
-        path = os.path.join(os.getcwd(), 'Wrye Mash.txt')
-        txt  = open(path).read()
-        self.htmlContent.SetHtmlData(txt)
-        self.htmlText.SetHtmlData(txt)
-
-        wx.LayoutAlgorithm().LayoutWindow(self, right)
-
-    def OnSashDrag(self,event):
-        """Handle sash moved."""
-        wMin,wMax = 80,self.GetSizeTuple()[0]-80
-        sashPos = max(wMin,min(wMax,event.GetDragRect().width))
-        self.left.SetDefaultSize((sashPos,10))
-        wx.LayoutAlgorithm().LayoutWindow(self, self.right)
-        # screensList.picture.Refresh()
-        settings['mash.help.sashPos'] = sashPos
-
-
-    #--Window Closing
-    def OnCloseWindow(self, event):
-        """Handle window close event.
-        Remember window size, position, etc."""
-        settings['mash.help.show'] = False
-        if not self.IsIconized() and not self.IsMaximized():
-            settings['mash.help.pos'] = self.GetPosition()
-            settings['mash.help.size'] = self.GetSizeTuple()
-        self.Destroy()
-
-
-#-# This line sets the actual help browser.
-#-# TODO (or not): let the user chose between the 'classic' view
-#-#    or the 'helpbook' style.
-HelpBrowser = DCGHelpBrowser
-HelpBrowser = HtmlHelpBrowser
+from gui.helpbrowser import HelpBrowser
 
 #------------------------------------------------------------------------------
 class MashApp(wx.App):
@@ -3922,7 +3686,7 @@ class MashApp(wx.App):
         if settings['mash.journal.show']:
             JournalBrowser().Show()
         if settings.get('mash.help.show'):
-            HelpBrowser().Show()
+            HelpBrowser(mashFrame, images, settings).Show()
         #-# D.C.-G. for SettingsWindow
         if settings['mash.settings.show']:
             global settingsWindow
@@ -7263,10 +7027,9 @@ class App_Help(Link):
 
     def Execute(self,event):
         """Handle menu selection."""
-        if not helpBrowser: 
-            HelpBrowser().Show()
-            settings['mash.help.show'] = True
-        helpBrowser.Raise()
+        #if not helpBrowser: 
+        HelpBrowser(mashFrame, images, settings).Show()
+        settings['mash.help.show'] = True
 
 #-# Added D.C.-G. for SettingsWindow.
 # Settings Links -------------------------------------------------------------------
